b22eb34faefee0d81b80042ed7572baaefa6bf3b
harman <hka@marketlogicsoftware.com>
2020-02-21 14:45:40 +0530

    complete todo

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function TodoForm({addTodoItem}) {
     }
 
     return (
-        <form className ="todoForm" onSubmit={handleSubmit}> 
+        <form className ="todo-form" onSubmit={handleSubmit}> 
             <input 
                 type="text" 
                 className="input" 
@@ -25,19 +25,37 @@ function TodoForm({addTodoItem}) {
 
 export default function App() {
     const [todos, setTodos] = useState([
-        {text: 'Learn about React'},
-        {text: 'Meet friends for Lunch'},
-        {text: 'Design a cool to-do app'}
+        {text: 'Learn about React', isCompleted: false},
+        {text: 'Meet friends for Lunch', isCompleted: false},
+        {text: 'Design a cool to-do app', isCompleted: false}
     ]);
 
-    const Todo = ({todo}) => <div className="todo">{todo.text}<img onClick={e=> deleteTodos(todo)} className="delete-icon" src="blue-delete.png" width="20px" height="20px"/></div>
+    const Todo = ({todo}) => (
+      <div className="todo">
+        <span className={todo.isCompleted?"is-completed":""}>{todo.text}</span>
+        <div className="wrapper-icons">
+          <button className="complete-button" onClick={e=> completeTodos(todo)}>Complete</button>
+          <img onClick={e=> deleteTodos(todo)} className="delete-icon" src="blue-delete.png" width="20px" height="20px"/>
+        </div>
+      </div>
+    );
+
+    const completeTodos = item => {
+      const newTodos = todos.map(todo => {
+        if(todo.text === item.text){
+          todo.isCompleted= true;
+        }
+        return todo;
+      })
+      setTodos(newTodos);
+    }
 
     const deleteTodos = item => {
         const newTodos = todos.filter(todo => todo.text !== item.text)
         setTodos(newTodos)
     }
     const addTodos = item => {
-        const newTodos = [...todos,{text: item}];
+        const newTodos = [...todos,{text: item, isCompleted: false}];
         setTodos(newTodos)
     }
 
